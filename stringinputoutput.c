0151d64b2b6a4d3dec9aac2a9bfc04e98e401479
BBluePanda <142870384+BBluePanda@users.noreply.github.com>
2024-09-27 12:17:01 +0530

    Add files via upload

---
+++ stringinputoutput.c
@@ -0,0 +1,14 @@
+//program for string input output
+//bhargab rudra kalita,32
+//27 sept 24
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char fname[20];
+	printf("\n Enter first name:");
+	scanf("%s",fname);
+	printf("\nFirst Name:%s",fname);
+	return 0;
+}
